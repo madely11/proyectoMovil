fd2ba0a220ff8e1dd264a3ade507f636537b73b2
madely11 <mnbetancourt@espe.edu.ec>
2022-01-27 20:02:47 -0500

    login con firebase

--- Restaurante Proyecto/app/src/main/java/com/example/restauranteproyecto/MainActivity.java
+++ Restaurante Proyecto/app/src/main/java/com/example/restauranteproyecto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.restauranteproyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,23 +10,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
     Button iniciarSesion;
     EditText user, pass;
+    FirebaseAuth mAuth;
+    String email = "";
+    String contra = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         iniciarSesion = (Button)findViewById(R.id.btnLogin);
         user = (EditText)findViewById(R.id.txtUsername);
         pass = (EditText)findViewById(R.id.txtPassword);
+
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent inicio = new Intent(MainActivity.this, ListaMenu.class);
-
-                if(user.getText().toString().equals("") || pass.getText().toString().equals("")){
+                //Intent inicio = new Intent(MainActivity.this, ListaMenu.class);
+                email = user.getText().toString();
+                contra = pass.getText().toString();
+                if(!email.isEmpty() && !contra.isEmpty()){
+                    loginUser();
+                }
+                else{
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"El Usuario o la contraseña se encuentran vacios", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+                /*if(user.getText().toString().equals("") || pass.getText().toString().equals("")){
                     Toast toast1 = Toast.makeText(getApplicationContext(),"El Usuario o la contraseña se encuentran vacios", Toast.LENGTH_SHORT);
                     toast1.show();
                 }else{
@@ -36,15 +57,26 @@ public class MainActivity extends AppCompatActivity {
                             Toast toast1 = Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrectos", Toast.LENGTH_SHORT);
                             toast1.show();
                         }
-                }
-
-
-
-
-
+                }*/
 
             }
         });
 
+    }
+
+    private void loginUser(){
+        mAuth.signInWithEmailAndPassword(email, contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this, ListaMenu.class));
+                    finish();
+                }
+                else{
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrectos", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+            }
+        });
     }
 }
